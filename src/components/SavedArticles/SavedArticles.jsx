74b033ea1062352e152c957aa7d68e5b5ea41fd1
Aaron <dogking189@gmail.com>
2026-03-05 01:17:31 -0600

    Implement SavedArticles component with full article display

--- src/components/SavedArticles/SavedArticles.jsx
+++ src/components/SavedArticles/SavedArticles.jsx
@@ -1,15 +1,34 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import "../Articles/Articles.css";
 import "./SavedArticles.css";
 
-function SavedArticles( { savedArticles } ) {
+function SavedArticles({ savedArticles, userData }) {
   return (
     <div className="saved-articles-container">
       <h2>Saved Articles</h2>
+      <div className="saved-articles-header">
+        <p className="saved-articles-label">Saved articles</p>
+
+        <h2 className="saved-articles-title">
+          {userData.name}, you have {savedArticles.length} saved articles
+        </h2>
+
+        <p className="saved-articles-keywords">
+          By keywords: Nature, Yellowstone, and 2 other
+        </p>
+      </div>
       {savedArticles.length > 0 ? (
-        <ul>
+        <ul className="saved-articles-grid">
           {savedArticles.map((article, index) => (
-            <li key={index}>{article.title}</li>
+            <li key={index} className="article-card">
+              <img className="article-image" src={article.urlToImage} />
+              <p className="article-date">
+                {new Date(article.publishedAt).toLocaleDateString()}
+              </p>
+              <h3 className="article-title">{article.title}</h3>
+              <p className="article-description">{article.description}</p>
+              <h1 className="article-source">{article.source.name}</h1>
+            </li>
           ))}
         </ul>
       ) : (
